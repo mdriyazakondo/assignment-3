37bb797e5eb763777c257105c65afcbee2b758b8
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-08-03 23:38:25 +0600

    first commit

---
+++ problem-1.js
@@ -0,0 +1,21 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 800;
+//write your code here
+
+if (area > 0) {
+  const divide = area / 2;
+  console.log(divide);
+}
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+
+if (money >= 25000) {
+  console.log("Laptop");
+} else if (money >= 10000) {
+  console.log("Cycle");
+} else {
+  console.log("Chocolate");
+}
+
